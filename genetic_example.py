43826a607731da28fc7390fc660017a47b8129c2
Konudula <Kishore.Konudula@havi.com>
2017-05-10 14:33:03 -0500

    changes to parameters for testing

--- genetic_example.py
+++ genetic_example.py
@@ -91,9 +91,10 @@ p_count = 100
 i_length = 5
 i_min = 0
 i_max = 100
+iterations = 100
 p = population(p_count, i_length, i_min, i_max)
 fitness_history = [grade(p, target)]
-for i in range(100):
+for i in range(iterations):
     p = evolve(p, target, i_min, i_max)
     fitness_history.append(grade(p, target))
 
